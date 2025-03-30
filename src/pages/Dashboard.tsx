327b092aea13eb7c6ad8096f4ddcf1e12596608c
CamiloArias12 <ariaspira13@gmail.com>
2025-03-30 13:21:48 -0500

    delete files

--- src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Dashboard() {
-
-  return (
-    <div className='flex flex-col w-full h-full justify-center items-center gap-4'>
-      <h1 className=' text-2xl'>Dashboard</h1>
-    </div>
-  );
-}
